b56b0f4ec2ff94c4fc9d74c4f04b9a7ed3d0bc1c
Tero Jäntti <tkjantti@gmail.com>
2018-11-16 21:08:30 +0200

    Map class.

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,7 @@
 /* global CPlayer, maps */
 /* global VECTOR */
 /* global SONGS */
+/* global Map */
 
 (function () {
     const clamp = VECTOR.clamp;
@@ -45,13 +46,6 @@
     const TILE_GROUND = 1;
     const TILE_WALL = 3;
     const TILE_BLOCKER = 4;
-
-    const ONLINE_TOGGLE_DELAY = 1200;
-
-    const LAYER_GROUND = 'G';
-    const LAYER_FLASHING = 'F';
-    const LAYER_BLOCKERS = 'B';
-    const LAYER_WALLS = 'W';
 
     const HELP_TEXT_DISPLAY_TIME = 3000;
 
@@ -84,44 +78,21 @@
         ""
     ];
 
-    let cx; // Convas context
+    let cx; // Canvas context
 
     let lives = MAX_LIVES;
 
     let mapIndex = 0;
     let currentMap;
-
-    let tileEngine;
-
-    let sprites = [];
+    let map = new Map();
 
     let player;
 
     let artifactCount;
     let numberOfArtifactsCollected = 0;
 
-    let online;
-
-    // When online mode was requested on/off (it takes a little time to toggle it).
-    let onlineToggleSwitchTime;
-
-    // Last time when online mode was toggled on/off.
-    let onlineLatestToggleTime;
-
-    // How long to wait until next on/off toggle.
-    let onlineToggleWaitTime;
-
-    let levelStartTime;
-
     // Angle for the ghosts in the winning animation.
     let ghostAngle = 0;
-
-    function resetLevel() {
-        sprites = [];
-        online = true;
-        onlineToggleSwitchTime = null;
-        onlineLatestToggleTime = levelStartTime = performance.now();
-    }
 
     function mapIsFinished() {
         return numberOfArtifactsCollected === artifactCount;
@@ -138,20 +109,6 @@
         let toY = spriteTo.y + spriteTo.height / 2;
 
         return kontra.vector(toX - fromX, toY - fromY);
-    }
-
-    function collidesWithLayer(sprite, layer) {
-        let cameraCoordinateBounds = {
-            x: -tileEngine.sx + sprite.x,
-            y: -tileEngine.sy + sprite.y,
-            width: sprite.width,
-            height: sprite.height
-        };
-        return tileEngine.layerCollidesWith(layer, cameraCoordinateBounds);
-    }
-
-    function collidesWithBlockers(sprite) {
-        return online && collidesWithLayer(sprite, LAYER_BLOCKERS);
     }
 
     function createArtifact(position) {
@@ -175,7 +132,7 @@
             update() {
                 let movement = null;
 
-                if (collidesWithBlockers(this)) {
+                if (map.collidesWithBlockers(this)) {
                     this.color = 'yellow';
                     let randomDirection = kontra.vector(
                         (-0.5 + Math.random()) * 20,
@@ -193,7 +150,7 @@
                     } else {
                         movement = this._target.minus(this.position).normalized();
                     }
-                } else if (gameIsFinished() && 1500 < (performance.now() - levelStartTime)) {
+                } else if (gameIsFinished() && 1500 < (performance.now() - map.startTime)) {
                     let angle = ghostAngle + this.number * 0.3;
                     let r = 180 + Math.sin(ghostAngle * 10) * 30;
                     let target = player.position.plus(
@@ -214,7 +171,7 @@
                         height: this.height,
                     };
 
-                    if (!collidesWithBlockers(newBounds)) {
+                    if (!map.collidesWithBlockers(newBounds)) {
                         this.move(movement);
                     } else {
                         let newTarget = kontra.vector(this.x, this.y);
@@ -242,8 +199,8 @@
             // Moves by the given vector, keeping within level bounds.
             move(movement) {
                 let newPosition = kontra.vector(
-                    clamp(this.x + movement.x, 0, tileEngine.mapwidth - this.width),
-                    clamp(this.y + movement.y, 0, tileEngine.mapheight - this.height));
+                    clamp(this.x + movement.x, 0, map.width - this.width),
+                    clamp(this.y + movement.y, 0, map.height - this.height));
                 this.position = newPosition;
             },
 
@@ -264,7 +221,7 @@
                 // When offline, approach the player from different
                 // directions so that the ghosts don't slump together
                 // and give a feeling of surrounding the player.
-                if (!online && distanceToPlayer > 140) {
+                if (!map.online && distanceToPlayer > 140) {
                     target.x += this.relativeDir.x * 130;
                     target.y += this.relativeDir.y * 130;
                 }
@@ -309,34 +266,34 @@
 
                 let newBounds = {
                     // keep within the map
-                    x: clamp(this.x + xDiff, 0, tileEngine.mapwidth - this.width),
-                    y: clamp(this.y + yDiff, 0, tileEngine.mapheight - this.height),
+                    x: clamp(this.x + xDiff, 0, map.width - this.width),
+                    y: clamp(this.y + yDiff, 0, map.height - this.height),
 
                     width: this.width,
                     height: this.height,
                 };
 
-                if (!collidesWithLayer(newBounds, LAYER_WALLS)) {
+                if (!map.collidesWithWalls(newBounds)) {
                     this.position = kontra.vector(newBounds.x, newBounds.y);
                 } else if (xDiff && yDiff) {
                     // Check if can move horizontally.
                     newBounds = {
-                        x: clamp(this.x + xDiff, 0, tileEngine.mapwidth - this.width),
+                        x: clamp(this.x + xDiff, 0, map.width - this.width),
                         y: this.y,
                         width: this.width,
                         height: this.height,
                     };
-                    if (!collidesWithLayer(newBounds, LAYER_WALLS)) {
+                    if (!map.collidesWithWalls(newBounds)) {
                         this.position = kontra.vector(newBounds.x, newBounds.y);
                     } else {
                         // Check if can move vertically.
                         newBounds = {
                             x: this.x,
-                            y: clamp(this.y + yDiff, 0, tileEngine.mapheight - this.height),
+                            y: clamp(this.y + yDiff, 0, map.height - this.height),
                             width: this.width,
                             height: this.height,
                         };
-                        if (!collidesWithLayer(newBounds, LAYER_WALLS)) {
+                        if (!map.collidesWithWalls(newBounds)) {
                             this.position = kontra.vector(newBounds.x, newBounds.y);
                         }
                     }
@@ -365,60 +322,58 @@
         return map.data.reduce((total, current) => total + current).split('').map(convert);
     }
 
-    function createMap(map) {
-        resetLevel();
-
-        currentMap = map;
-
-        onlineToggleWaitTime = map.online;
+    function createMap(mapData) {
+        currentMap = mapData;
 
         const blockerData = mapToLayer(
-            map, tile => (tile === '#' || tile === 'A') ? TILE_BLOCKER : 0);
+            mapData, tile => (tile === '#' || tile === 'A') ? TILE_BLOCKER : 0);
 
-        tileEngine = kontra.tileEngine({
+        let tileEngine = kontra.tileEngine({
             tilewidth: TILE_WIDTH,
             tileheight: TILE_HEIGHT,
-            width: map.data[0].length,
-            height: map.data.length,
+            width: mapData.data[0].length,
+            height: mapData.data.length,
             tilesets: [{
                 firstgid: 1,
                 image: kontra.assets.images.tilesheet,
             }],
             layers: [{
-                name: LAYER_GROUND,
+                name: Map.LAYER_GROUND,
                 data: mapToLayer(
-                    map,
+                    mapData,
                     tile => (tile === ' ' || tile === 'G' || tile === '@' || tile === 'a') ? TILE_GROUND : 0),
             }, {
-                name: LAYER_WALLS,
-                data: mapToLayer(map, tile => tile === '=' ? TILE_WALL : 0),
+                name: Map.LAYER_WALLS,
+                data: mapToLayer(mapData, tile => tile === '=' ? TILE_WALL : 0),
             }, {
-                name: LAYER_FLASHING,
+                name: Map.LAYER_FLASHING,
                 data: blockerData,
             }, {
-                name: LAYER_BLOCKERS,
+                name: Map.LAYER_BLOCKERS,
                 data: blockerData,
             }],
         });
 
-        let playerPosition = findPositionsOf(map, '@')[0];
+        map.reset(mapData, tileEngine);
+
+        let playerPosition = findPositionsOf(mapData, '@')[0];
         playerPosition.x += 5;
         player = createPlayer(playerPosition);
-        sprites.push(player);
+        map.add(player);
 
-        let artifactPositions = findPositionsOf(map, 'A').concat(findPositionsOf(map, 'a'));
+        let artifactPositions = findPositionsOf(mapData, 'A').concat(findPositionsOf(mapData, 'a'));
         artifactCount = artifactPositions.length;
         numberOfArtifactsCollected = 0;
         artifactPositions.forEach(pos => {
             pos.x += 5;
             pos.y += 5;
             let artifact = createArtifact(pos);
-            sprites.push(artifact);
+            map.add(artifact);
         });
 
-        findPositionsOf(map, 'G').forEach((pos, i) => {
+        findPositionsOf(mapData, 'G').forEach((pos, i) => {
             let ghost = createGhost(pos, i);
-            sprites.push(ghost);
+            map.add(ghost);
         });
     }
 
@@ -461,26 +416,9 @@
         });
     }
 
-    function adjustCamera() {
-        const margin = 200;
-        const cameraSpeed = playerSpeed;
-
-        if (player.x - tileEngine.sx < margin) {
-            tileEngine.sx -= cameraSpeed;
-        } else if ((tileEngine.sx + kontra.canvas.width) - player.x < margin) {
-            tileEngine.sx += cameraSpeed;
-        }
-
-        if (player.y - tileEngine.sy < margin) {
-            tileEngine.sy -= cameraSpeed;
-        } else if ((tileEngine.sy + kontra.canvas.height) - player.y < margin) {
-            tileEngine.sy += cameraSpeed;
-        }
-    }
-
     function checkCollisions() {
-        for (let i = 0; i < sprites.length; i++) {
-            let sprite = sprites[i];
+        for (let i = 0; i < map.entities.length; i++) {
+            let sprite = map.entities[i];
 
             if ((sprite.type === 'ghost') &&
                 (sprite.color !== 'yellow') &&
@@ -504,57 +442,22 @@
     function createGameLoop() {
         return kontra.gameLoop({
             update() {
-                for (let i = 0; i < sprites.length; i++) {
-                    let sprite = sprites[i];
-                    sprite.update();
-                }
+                map.update();
+                map.adjustCamera(player, playerSpeed);
 
                 checkCollisions();
-
-                adjustCamera();
-
-                let now = performance.now();
-
-                if (onlineToggleWaitTime < (now - onlineLatestToggleTime)) {
-                    onlineToggleSwitchTime = now;
-                    onlineLatestToggleTime = now;
-                    onlineToggleWaitTime = online ? currentMap.offline : currentMap.online;
-                }
-
-                if (onlineToggleSwitchTime &&
-                    ONLINE_TOGGLE_DELAY < (now - onlineToggleSwitchTime)) {
-                    online = !online;
-                    onlineToggleSwitchTime = null;
-                }
 
                 if (mapIsFinished() && (mapIndex < maps.length - 1)) {
                     createMap(maps[++mapIndex]);
                 }
 
-                sprites = sprites.filter(s => !s.dead);
-
                 ghostAngle += 0.005; // Update winning animation
             },
 
             render() {
-                let time = performance.now() - levelStartTime;
+                let time = performance.now() - map.startTime;
 
-                tileEngine.renderLayer(LAYER_GROUND);
-                tileEngine.renderLayer(LAYER_WALLS);
-                if (onlineToggleSwitchTime && (Math.random() >= 0.5)) {
-                    tileEngine.renderLayer(LAYER_FLASHING);
-                }
-                if (online && !onlineToggleSwitchTime) {
-                    tileEngine.renderLayer(LAYER_BLOCKERS);
-                }
-
-                cx.save();
-                cx.translate(-tileEngine.sx, -tileEngine.sy);
-                for (let i = 0; i < sprites.length; i++) {
-                    let sprite = sprites[i];
-                    sprite.render();
-                }
-                cx.restore();
+                map.render(cx);
 
                 if (artifactCount) {
                     drawStatusText(cx, `A: ${numberOfArtifactsCollected} / ${artifactCount}             L: ${lives}`);
